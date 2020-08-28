75f8b2df4e657df8ed8a75b1b984f7399f72911f
play.js <git@gituser.com>
2020-08-27 22:28:42 -0400

    Added Express Server

---
+++ index.js
@@ -0,0 +1,18 @@
+const express = require("express");
+const path = require("path");
+
+const app = express();
+
+const port = process.env.PORT || 3001;
+
+
+app.get('/', (req, res) => {
+  res.sendFile(path.join(__dirname,'public','index.html'))
+})
+
+
+app.listen(port, () => {
+  console.log(`Example app listening at http://localhost:${port}`)
+})
+
+app.use(express.static('public'))
